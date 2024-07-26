37d9e66bb9328badab3f22d92488faffafbe759e
foufou <rachidfourat2@gmail.com>
2024-07-26 12:42:26 +0100

    Implement role-based authentication system with Spring Boot

---
+++ src/main/java/com/fourat/authapi/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.fourat.authapi.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoginDto {
+    private String email ;
+    private String password ;
+}
---
+++ src/main/java/com/fourat/authapi/dto/RegisterDto.java
@@ -0,0 +1,15 @@
+package com.fourat.authapi.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import java.io.Serializable;
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RegisterDto implements Serializable {
+    String firstName ;
+    String lastName ;
+    String email;
+    String password ;
+    String userRole ;
+}
